6e85e3ab4bf5fede68685bc7a74839a7ac2da85e
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-04-27 14:04:04 +0700

    add project

---
+++ src/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
